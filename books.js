bebaa816dac37f6a85649bc34366fe7fa2836909
Serhii T <tocarchuk@gmail.com>
2021-10-25 13:43:29 +0300

    Initial commit

---
+++ books.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, status){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.info = function() {
+    return (title + ' by ' + author + ', ' + pages + ' pages, ' + status);
+  }
+}
+
