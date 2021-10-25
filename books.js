dc9e80b1b7ca32a971bf58e5492819cb17d990e6
Serhii T <tocarchuk@gmail.com>
2021-10-25 22:35:07 +0300

    Create Form to add book from UI

--- books.js
+++ books.js
@@ -1,16 +1,32 @@
+const form = document.getElementById('new-book-form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+
 let myLibrary = [];
 
-function Book(title, author, pages, status){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-  this.info = function() {
-    return (title + ' by ' + author + ', ' + pages + ' pages, ' + status);
+class Book{
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+      return (title + ' by ' + author + ', ' + pages + ' pages, ' + status);
+    }
   }
 }
 
-function addBookToLibrary(){
-  
+function addBookToLibrary(book){
+  myLibrary.push(book);
+  console.log(myLibrary);
 }
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const newBook = new Book(title.value, author.value,pages.value, read.value);
+  addBookToLibrary(newBook);
+});
 
